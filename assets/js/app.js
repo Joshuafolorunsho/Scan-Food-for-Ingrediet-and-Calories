21eb52c959f33a9f1232a4cd727aa5eee12378f9
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-07-14 12:28:26 +0100

    make it case insensitive

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ notifyBtn.addEventListener('click', () => {
 });
 
 function validateEmail(email) {
-   const re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+   const re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
    return re.test(email);
  }
  
