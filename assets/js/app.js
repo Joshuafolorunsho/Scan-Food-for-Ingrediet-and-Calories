a8615e5b203bc5193d4645292d1f7231609c33ce
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-07-15 23:27:56 +0100

    make carousel work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,95 @@
-const validateBtn = document.querySelector('#validate');
-const form = document.querySelector('#subscribe-form');
-const notifyBtn = document.querySelector('#notify-me');
-const emailInput = document.querySelector('#email');
+const validateBtn = document.querySelector("#validate");
+const form = document.querySelector("#subscribe-form");
+const notifyBtn = document.querySelector("#notify-me");
+const emailInput = document.querySelector("#email");
 
-
-notifyBtn.addEventListener('click', () => {
-   form.classList.add('d-block');
-   notifyBtn.classList.add('d-none');
+notifyBtn.addEventListener("click", () => {
+  form.classList.add("d-block");
+  notifyBtn.classList.add("d-none");
 });
 
 function validateEmail(email) {
-   const re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
-   return re.test(email);
- }
- 
- function validate() {
-   const $result = $("#result");
-   const email = $("#email").val();
-   $result.text("");
- 
-   if (validateEmail(email)) {
-     $result.text("Successful. We'll notify you when our app is launched");
-     $result.css("color", "green");
-     setTimeout(() => {
-      $result.css('display', 'none');
-     }, 2000)
-   } else {
-      if (email.length == 0) {
-         $result.text('Field cannot be left empty, try again')
-         $result.css("color", "red");
-         setTimeout(() => {
-            $result.text('');
-           }, 2000)
-      } else {
-         $result.text("Invalid email, try again");
-         $result.css("color", "red");
-         setTimeout(() => {
-            $result.css('display', 'none');
-           }, 2000);
-      }
-   }
-   return false;
- }
+  const re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+  return re.test(email);
+}
 
-form.addEventListener('submit', (event) => {
-   event.preventDefault();
-   validate();
-})
+function validate() {
+  const $result = $("#result");
+  const email = $("#email").val();
+  $result.text("");
+
+  if (validateEmail(email)) {
+    $result.text("Successful. We'll notify you when our app is launched");
+    $result.css("color", "green");
+    setTimeout(() => {
+      $result.css("display", "none");
+    }, 2000);
+  } else {
+    if (email.length == 0) {
+      $result.text("Field cannot be left empty, try again");
+      $result.css("color", "red");
+      setTimeout(() => {
+        $result.text("");
+      }, 2000);
+    } else {
+      $result.text("Invalid email, try again");
+      $result.css("color", "red");
+      setTimeout(() => {
+        $result.css("display", "none");
+      }, 2000);
+    }
+  }
+  return false;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  validate();
+});
+
+/* Showcase */
+
+const state = {};
+const carouselList = document.querySelector(".carousel__list");
+const carouselItems = document.querySelectorAll(".carousel__item");
+const elems = Array.from(carouselItems);
+
+carouselList.addEventListener("click", function (event) {
+  var newActive = event.target;
+  var isItem = newActive.closest(".carousel__item");
+
+  if (!isItem || newActive.classList.contains("carousel__item_active")) {
+    return;
+  }
+
+  update(newActive);
+});
+
+const update = function (newActive) {
+  const newActivePos = newActive.dataset.pos;
+
+  const current = elems.find((elem) => elem.dataset.pos == 0);
+  const prev = elems.find((elem) => elem.dataset.pos == -1);
+  const next = elems.find((elem) => elem.dataset.pos == 1);
+  const first = elems.find((elem) => elem.dataset.pos == -2);
+  const second = elems.find((elem) => elem.dataset.pos == 2);
+  const last = elems.find((elem) => elem.dataset.pos == 3);
+
+  current.classList.remove("carousel__item_active");
+
+  [current, prev, next, first, second, last].forEach((item) => {
+    var itemPos = item.dataset.pos;
+
+    item.dataset.pos = getPos(itemPos, newActivePos);
+  });
+};
+
+const getPos = function (current, active) {
+  const diff = current - active;
+
+  if (Math.abs(current - active) > 2) {
+    return -current;
+  }
+
+  return diff;
+};
