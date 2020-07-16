8b713901cf1caa13b98cc784a9a053221f3151a6
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-07-16 15:14:02 +0100

    markup: work more on the markup

--- index.php
+++ index.php
@@ -4,10 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Scan Food for Ingrediets and Calories</title>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-      rel="stylesheet"
-    />
+    <meta name="description" content="fScan is an application that helps you scan your food and provides you a detailed review of calories and ingrediets in the food">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/bootstrap.css" />
     <link rel="stylesheet" href="assets/css/main.css" />
   </head>
@@ -29,48 +27,57 @@
         </nav>
       </div>
       <section class="header-section">
-
-              <div class="header__info">
-                <h5
-                  class="header__title d-flex align-items-center text-uppercase mb-4"
-                >
-                  We are launching soon
-                </h5>
-                <h1 class="header__description mb-4">
-                  Easiest way to identify your allergens with just a click Using
-                  our food scan app
-                </h1>
-                <button
-                  class="btn btn-custom d-flex align-items-center"
-                  id="notify-me"
-                >
-                  Notify me
-                  <img
-                    class="pl-2"
-                    src="./assets/images/right-arrow.svg"
-                    alt="arrow right"
-                  />
-                </button>
-              </div>
+            <div class="container">
+               <div class="row align-items-center">
+                  <div class="col-md-5 col-lg-6">
+                     <div class="header__info">
+                       <h5
+                         class="header__sub-title d-flex align-items-center text-uppercase mb-4"
+                       >
+                         We are launching soon
+                       </h5>
+                       <h1 class="header__title heading-1 mb-4 font-weight-bold">
+                        Ever Worried about What is in your Food?
+                       </h1>
+                       <p class="header__description">Take control of what you eat. Track calories and breakdown ingredients with fSacn.</p>
+                       <button
+                         class="btn btn-custom font-weight-bold d-inline-block justify-content-start align-items-center"
+                         id="notify-me"
+                       >
+                         Notify me
+                         <img
+                           class="pl-2"
+                           src="./assets/images/right-arrow.svg"
+                           alt="arrow right"
+                         />
+                       </button>
+                     </div>
+                  </div>
+                  <div class="d-none d-md-block col-md-7 col-lg-6">
+                     <img class="hero-img" class="img-fluid" src="./assets/images/fscan home screen.svg" alt="fscan home screen">
+                  </div>
+               </div>
+            </div>
             
-      </section>
-    </header>
+         </section>
+      </header>
+      <button id="goTop">top</button>
     <main>
       <section class="about py-5">
         <div class="container">
           <div class="row align-items-center">
             <div class="col-sm-6">
-              <img class="img-fluid" src="./assets/images/phone.svg" alt="" />
+              <img class="img-fluid" src="./assets/images/phone.svg" alt="phone mockup" />
             </div>
-            <div class="col-sm-6">
-              <h2 class="text-middle">Say No More to Unhealthy Food</h2>
-              <p class="py-2">
+            <div class="col-sm-6 pt-5 py-sm-0">
+              <h2 class="text-middle color-blue heading-2 font-weight-bold text-center text-sm-left">Say No More to Unhealthy Food</h2>
+              <p class="py-2 text-center text-sm-left heading-4">
                 Our goal is to bring tansparency to the food industry and
                 providing valuable and much-needed information readily available
                 at your fingertips to make better informed choices and promote
                 healthy living.
               </p>
-              <p>
+              <p class="text-center text-sm-left heading-4">
                 We are bringing you a completely free app that allows you to
                 scan / search your food products and obtain the nutritional
                 values in it.
@@ -81,12 +88,12 @@
       </section>
       <section class="features">
         <div class="container">
-          <div class="row my-5">
+          <div class="row mt-2 mb-5 my-sm-5">
             <div class="col">
-              <h2 class="text-middle text-center features__title color-blue">
+              <h2 class="text-middle text-center features__title color-blue heading-2 font-weight-bold">
                 Amazing benefits of fScan
               </h2>
-              <p class=" text-middle text-center">
+              <p class=" text-middle text-center heading-4 py-2">
                 Easy to use, convenient and suitable for all age groups, our
                 food scan app is equipped with benefits to keep you healthy.
               </p>
@@ -104,10 +111,10 @@
                     alt="checkmark orange"
                   />
                 </div>
-                <h6 class="feature__sub-title py-3">
+                <h6 class="feature__sub-title py-3 heading-3 color-blue">
                   Never Worry About what you eat
                 </h6>
-                <p class="feature__sub-description">
+                <p class="feature__sub-description heading-4">
                   With just a few clicks, you can scan unlimited number of food
                   based on your diet with our advanced scan and get instant
                   detailed overview on your scan.
@@ -123,8 +130,8 @@
                     alt="checkmark orange"
                   />
                 </div>
-                <h6 class="feature__sub-title py-3">Track your Diet</h6>
-                <p class="feature__sub-description">
+                <h6 class="feature__sub-title py-3 heading-3 color-blue">Track your Diet</h6>
+                <p class="feature__sub-description heading-4">
                   With just a single click, you can now track your your calories
                   and nutrients and maintain a healthy living, making you enjoy
                   your favorite foods without feeling guilty
@@ -142,8 +149,8 @@
                     alt="checkmark orange"
                   />
                 </div>
-                <h6 class="feature__sub-title py-3 color-blue">Eat Healthy and More</h6>
-                <p class="feature__sub-description">
+                <h6 class="feature__sub-title py-3 heading-3 color-blue">Eat Healthy and More</h6>
+                <p class="feature__sub-description heading-4">
                   With your allergens in mind, we are not just bringing the
                   option of scanning your food, but the option of giving you
                   essential healtier choices base on your allergens.
@@ -157,56 +164,56 @@
         <div class="container">
           <div class="row align-items-center">
             <div class="col-md-8">
-              <h3 class="how-it-works__title color-blue">How fScan works</h3>
-              <p>
+              <h3 class="how-it-works__title color-blue heading-2 font-weight-bold heading-2 text-center text-md-left">How fScan works</h3>
+              <p class="heading-4 text-center text-md-left pb-2">
                 With this few steps, you can enjoy your food without wondering
                 what is in it.
               </p>
               <div class="how-it-works__processes">
                 <div class="how-it-works__process">
-                  <img src="./assets/images/1.svg" alt="process one" />
-                  <h5 class="how-it-works__sub-title">
+                  <img class="lazyload" data-src="./assets/images/1.svg" alt="process one" />
+                  <h5 class="how-it-works__sub-title heading-3">
                     Scan unlimited number of food
                   </h5>
-                  <p class="how-it-works__sub-description">
+                  <p class="how-it-works__sub-description heading-4">
                     Find out what is in your food with just glance using our app
                   </p>
                 </div>
                 <div class="how-it-works__process">
-                  <img src="./assets/images/2.svg" alt="process two" />
-                  <h5 class="how-it-works__sub-title">
+                  <img class="lazyload" data-src="./assets/images/2.svg" alt="process two" />
+                  <h5 class="how-it-works__sub-title heading-3">
                     Log your meals in seconds.
                   </h5>
-                  <p class="how-it-works__sub-description">
+                  <p class="how-it-works__sub-description heading-4">
                     You can also scan food from your gallery and get instant
                     overview with our advanced image recognition.
                   </p>
                 </div>
                 <div class="how-it-works__process">
-                  <img src="./assets/images/3.svg" alt="process three" />
-                  <h5 class="how-it-works__sub-title">
+                  <img class="lazyload" data-src="./assets/images/3.svg" alt="process three" />
+                  <h5 class="how-it-works__sub-title heading-3">
                     Get the result of your scan
                   </h5>
-                  <p class="how-it-works__sub-description">
+                  <p class="how-it-works__sub-description heading-4">
                     With a click, instantly you will get result of calories,
                     macros and micronutrients
                   </p>
                 </div>
                 <div class="how-it-works__process">
-                  <img src="./assets/images/4.svg" alt="process four" />
-                  <h5 class="how-it-works__sub-title">
+                  <img class="lazyload" data-src="./assets/images/4.svg" alt="process four" />
+                  <h5 class="how-it-works__sub-title heading-3">
                     Scan unlimited number of food
                   </h5>
-                  <p class="how-it-works__sub-description">
+                  <p class="how-it-works__sub-description heading-4">
                     Find out what is in your food with just glance using our app
                   </p>
                 </div>
               </div>
             </div>
             <div class="col-md-4 d-none d-md-block">
-              <img
-                class="img-fluid"
-                src="./assets/images/phone-scanning-food-with-edge.svg"
+              <img 
+                class="img-fluid lazyload"
+                data-src="./assets/images/phone-scanning-food-with-edge.svg"
                 alt="screenshot of app scanning food"
               />
             </div>
@@ -218,24 +225,24 @@
           <div class="row align-items-center">
             <div class="col-sm-6 mx-auto">
               <div class="other-benefits__card py-4">
-                <h4 class="other-benefits__title text-center">Other Benefits</h4>
+                <h4 class="heading-3 other-benefits__title text-center">Other Benefits</h4>
                 <div class="mx-auto rounded-circle other-benefits__logo d-flex align-items-center my-4">
-                  <img src="./assets/images/benefit fScan logo.svg" />
+                  <img class="lazyload" data-src="./assets/images/benefit fScan logo.svg" alt="benefit fscan logo" />
                 </div>
                 <ul class="pt-1 mx-auto other-benefits__lists list-unstyled">
-                  <li class="other-benefits__item">It is <span>FREE</span></li>
-                  <li class="other-benefits__item">Make better choices on your diet</li>
-                  <li class="other-benefits__item">Know what you can actually consume</li>
-                  <li class="other-benefits__item">Visualizing is energizing</li>
+                  <li class="other-benefits__item heading-4">It is <span>FREE</span></li>
+                  <li class="other-benefits__item heading-4">Make better choices on your diet</li>
+                  <li class="other-benefits__item heading-4">Know what you can actually consume</li>
+                  <li class="other-benefits__item heading-4">Visualizing is energizing</li>
                 </ul>
               </div>
             </div>
             <div class="col-sm-6">
-              <h2 class="benefits__title color-blue mb-3">
+              <h2 class="benefits__title color-blue heading-2 font-weight-bold mb-3 text-center text-sm-left pt-3 pt-sm-0">
                 Create your own health journey
               </h2>
-              <p>
-                to helo you reach your health goals and customize your health
+              <p class="heading-4">
+                To help you reach your health goals and customize your health
                 journey, you can now add your favorites food scans to your diary
                 to keep track of your daily intake.
               </p>
@@ -245,10 +252,10 @@
       </section>
       <section class="showcase py-5">
         <div class="container">
-          <h2 class="showcase__title text-center color-blue">
+          <h2 class="showcase__title text-center color-blue heading-2 font-weight-bold">
             Screenshots from our Awesome App
           </h2>
-          <p class="showcase__description text-center text-middle">
+          <p class="showcase__description text-center text-middle heading-4">
             With our customer at heart, we are making sure our app is easy to
             navigate, helping you achieve your goals easier and faster.
           </p>
@@ -273,10 +280,10 @@
           <div class="subscribe-text">
             <div class="row">
               <div class="col">
-                <h2 class="subscribe__title text-center">
+                <h2 class="subscribe__title text-center color-blue heading-2 font-weight-bold">
                   Don't Hesitate To Subscribe
                 </h2>
-                <p class="subscribe__description text-middle text-center">
+                <p class="subscribe__description text-middle text-center heading-4 mb-4">
                   Subscribe to get notified on when our app will be launched and
                   be the first to get our exclusive premium access to unlimited
                   health care services.
@@ -287,7 +294,7 @@
               <div class="col-12">
                 <form action="index.php" method="post" id="subscribe-form">
                   <div class="position-relative">
-                    <!-- <label for="email">Email</label> -->
+                    <label class="hide" for="email">Email</label>
                     <input
                       type="text"
                       class="email-input"
@@ -298,7 +305,7 @@
                     <input
                       type="submit"
                       value="Notify me"
-                      class="btn btn-custom submit position-absolute"
+                      class="btn btn-custom font-weight-bold submit position-absolute"
                       name="submit"
                       id="validate"
                     />
@@ -336,33 +343,32 @@
         </div>
       </section>
     </main>
-
-    <footer class="footer">
+    <footer class="footer text-center py-4 py-sm-2">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-sm-4">
-            <img class="logo" src="./assets/images/fscan-logo.svg" alt="" />
+            <img class="logo lazyload" data-src="./assets/images/fscan-logo.svg" alt="fscan logo" />
           </div>
           <div class="col-sm-4">
-            <p class="text-center">
+            <p class="pt-3">
               Copyright &copy; 2020 fScan. All rights reserved
             </p>
           </div>
           <div class="col-sm-4">
-            <ul class="list-unstyled d-flex">
+            <ul class="list-unstyled d-flex justify-content-center align-items-center pt-3">
               <li class="pr-3">
-                <a href="https://www.twitter.com" target="_blank">
-                  <img src="./assets/images/twitter.svg" alt="twitter logo" />
+                <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+                  <img class="lazyload" data-src="./assets/images/twitter.svg" alt="twitter logo" />
                 </a>
               </li>
               <li class="pr-3">
-                <a href="https://www.linkedin.com" target="_blank">
-                  <img src="./assets/images/linkedin.svg" alt="linkedin logo" />
+                <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+                  <img class="lazyload" data-src="./assets/images/linkedin.svg" alt="linkedin logo" />
                 </a>
               </li>
               <li>
-                <a href="https://www.facebook.com" target="_blank">
-                  <img src="./assets/images/facebook.svg" alt="facebook logo" />
+                <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                  <img class="lazyload" data-src="./assets/images/facebook.svg" alt="facebook logo" />
                 </a>
               </li>
             </ul>
@@ -370,9 +376,11 @@
         </div>
       </div>
     </footer>
-    <script src="./assets/js/jquery.js"></script>
-    <script src="./assets/js/popper.js"></script>
-    <script src="./assets/js/bootstrap.js"></script>
-    <script defer src="./assets/js/app.js"></script>
+    <div id="subscribe"></div>
+    <script src="./assets/js/lazysizes.min.js" async></script>
+    <script src="./assets/js/jquery.js" async defer></script>
+    <script src="./assets/js/popper.js" async defer></script>
+    <script src="./assets/js/bootstrap.js" async defer></script>
+    <script async defer src="./assets/js/app.js"></script>
   </body>
 </html>
